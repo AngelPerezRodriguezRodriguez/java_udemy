2810cecde5dbedb7a15ddaf5495437593d0ca555
Angel Pérez Rodríguez Rodríguez <angelrodriguez46@aragon.unam.mx>
2023-03-17 23:39:08 -0600

    feat(007_clases): nuevo objeto 'Persona' llamado 'clienteDos'

--- fundamentos/007_clases/01_clases/src/clases/Persona.java
+++ fundamentos/007_clases/01_clases/src/clases/Persona.java
@@ -1,11 +1,12 @@
 
 package clases;
+// Indica que la clase se pertenece al paquete con nombre 'clases'
 
 /**
  *
  * @author Angel P.
  */
-
+ 
 public class Persona {
     
     //ATRIBUTOS
@@ -15,7 +16,7 @@ public class Persona {
     //MÉTODOS
     public void desplegarInformacion() {
         System.out.printf("%nNombre: %s%n",nombre);
-        System.out.printf("Apellido: %s%n%n",apellido);
+        System.out.printf("Apellido: %s%n",apellido);
         
         /* 
         Las variables "nombre" y "apellido" no importa que no estén definidas 
--- fundamentos/007_clases/01_clases/src/clases/PruebaPersona.java
+++ fundamentos/007_clases/01_clases/src/clases/PruebaPersona.java
@@ -17,7 +17,9 @@ public class PruebaPersona {
         new Persona()
         
         El constructor de nuestra clase "Persona" nos va a permitir crear 
-        objetos de la clase que hemos definido.
+        objetos de la clase que hemos definido. El método Persona() únicamente
+        se puede mandar a llamar al crear objetos junto con la palabra reservada
+        new.
         
         Estamos mandando a llamar el constructor "Persona()" predeterminado,
         que, aunque no lo definidos en nuestra clase "Persona", lo podemos 
@@ -26,17 +28,47 @@ public class PruebaPersona {
         El constructor nos va a permitir asignar valores al objeto desde el 
         momento de la creación del mismo y nos permite reservar memoria: las 
         variables "clienteUno" y los objetos "new Persona()" se almacenan en 
-        distintas partes de la memoria
+        distintas partes de la memoria.
         
         El constructor "Persona()" es un método especial que permite reservar
-        memoria para la creación y almacenamiiento del objeto. Una vez que se 
+        memoria para la creación y almacenamiento del objeto. Una vez que se 
         crea el objeto en memoria, regresa la referencia donde se a creado este 
-        objeto en memoria, esa referencia se le asigna a la variable "clenteUno"
+        objeto en memoria, esa referencia se le asigna a la variable "clienteUno".
         */
         
         clienteUno.nombre = "Angel";
         clienteUno.apellido = "Pérez";
-        
+
         clienteUno.desplegarInformacion();
+
+
+        Persona clienteDos = new Persona();
+
+        System.out.println();
+        System.out.println(clienteUno);
+        System.out.println(clienteDos);
+        /*
+        clases.Persona@63961c42
+        clases.Persona@65b54208
+
+        Se imprime el nombre del paquete, el nombre de la clase y después de '@' 
+        la referencia en memoria del objeto.
+
+        Todas nuestras variables se almacenan en la memoria RAM y JAVA, de manera
+        dinámica, reserva memoria; y la dirección de memoria es la que se asigna a
+        las variables para que se pueda acceder a estos objetos según la posición de 
+        memoria donde se almacenan
+        */
+
+        clienteDos.desplegarInformacion();
+        /*
+        Como en ningún momento especificamos los valores de los atributos de 'clienteDos'
+        se asigna el valor por default, en este caso null para tipo String
+        */
+
+        clienteDos.nombre = "Carla";
+        clienteDos.apellido = "Lara";
+
+        clienteDos.desplegarInformacion();
     }
 }
